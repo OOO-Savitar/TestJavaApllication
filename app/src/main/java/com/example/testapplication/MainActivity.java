290aedda99a030e12c975c634ac1207105383297
_-=Miyagi=-_ <ppamuzhakk@mail.ru>
2024-02-23 20:18:14 +0300

    Первое изменение

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -13,13 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("123", "321");
-            }
-    });
     }
 }
